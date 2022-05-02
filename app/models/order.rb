82845d64528581d1ce1c32acf2c1ad50ff7236ad
Takahiro Iizuka <agdjgt@gmail.com>
2022-05-02 16:57:27 +0900

    first draft for assignment

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+  belongs_to :customer, foreign_key: 'customers_id'
+  belongs_to :address, foreign_key: 'addresses_id'
+  has_many :order_foods
 end
--- app/models/order_food.rb
+++ app/models/order_food.rb
@@ -1,2 +1,4 @@
 class OrderFood < ApplicationRecord
+  belongs_to :order, foreign_key: 'orders_id'
+  belongs_to :food, foreign_key: 'foods_id'
 end
